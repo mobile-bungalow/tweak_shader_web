9b7476b60d70a9346edd729cb75421bba7967f91
mobile-bungalow <pwmay.work@gmail.com>
2025-05-19 15:11:36 -0700

    vim mode

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,28 +1,27 @@
-const DEFAULT_SHADER = `
-  #version 450
-  #pragma tweak_shader(version="1.0")
-  #pragma stage(compute)
+import { theme } from "./theme";
+const DEFAULT_SHADER = `#pragma stage(compute)
 
-  #pragma input(float, name=blue, default=0.0, min=0.0, max=1.0)
-  layout(set=1, binding=0) uniform custom_inputs {
-      float blue;
-  };
+#pragma input(float, name=blue, default=0.0, min=0.0, max=1.0)
+layout(set=1, binding=0) uniform custom_inputs {
+    float blue;
+};
 
-  #pragma target(name="output_image", screen)
-  layout(rgba8, set=0, binding=1) uniform writeonly image2D output_image;
+#pragma target(name="output_image", screen)
+layout(rgba8, set=0, binding=1) uniform writeonly image2D output_image;
 
-  layout(local_size_x = 16, local_size_y = 16) in;
-  void main() {
-      ivec2 pixel_coords = ivec2(gl_GlobalInvocationID.xy);
-      ivec2 image_size = imageSize(output_image);
+layout(local_size_x = 16, local_size_y = 16) in;
+void main() {
+    ivec2 pixel_coords = ivec2(gl_GlobalInvocationID.xy);
+    ivec2 image_size = imageSize(output_image);
 
-      vec2 normalized_coords = vec2(pixel_coords) / vec2(image_size);
+    vec2 normalized_coords = vec2(pixel_coords) / vec2(image_size);
 
-      vec4 color = vec4(normalized_coords.x, normalized_coords.y, blue, 1.0);
+    vec4 color = vec4(normalized_coords.x, normalized_coords.y, blue, 1.0);
 
-      vec4 inverse = vec4(vec3(1.0) - color.xyz, 1.0);
-      imageStore(output_image, pixel_coords, color);
-  }
+    vec4 inverse = vec4(vec3(1.0) - color.xyz, 1.0);
+    imageStore(output_image, pixel_coords, color);
+}
 `;
 
-export { DEFAULT_SHADER }
+
+export { DEFAULT_SHADER, theme }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,43 +1,96 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import { DEFAULT_SHADER } from "$lib";
+    import { DEFAULT_SHADER, theme } from "$lib";
     import init, { TweakShader, initialize_library } from "tweak_shader_wasm";
     import CodeMirror from "svelte-codemirror-editor";
+    import { Compartment } from "@codemirror/state";
+    import { vim } from "@replit/codemirror-vim";
     import { glsl } from "codemirror-lang-glsl";
 
+    let editor: CodeMirror;
     let canvas: HTMLCanvasElement;
+    let tweakShader: TweakShader;
 
     onMount(async () => {
         await init();
         let context = await initialize_library();
-        let tweakShader = new TweakShader(DEFAULT_SHADER, context);
+        tweakShader = new TweakShader(DEFAULT_SHADER, context);
+
+        canvas.width = 700;
+        canvas.height = 400;
         tweakShader.update_resolution(canvas.width, canvas.height);
         tweakShader.render(canvas);
     });
+
+    let vimMode = false;
+    const toggleVim = () => {
+        vimMode = !vimMode;
+    };
 </script>
 
 <main>
     <div class="container">
         <div class="leftColumn">
-            <canvas width="800" height="450" bind:this={canvas}></canvas>
+            <canvas bind:this={canvas}></canvas>
+            <div class="controls">
+                <div class="stats"></div>
+                <button aria-label="pause">Pause</button>
+                <div class="outputSelector"></div>
+            </div>
             <div class="inputs"></div>
         </div>
-        <CodeMirror value={DEFAULT_SHADER} lang={glsl()}></CodeMirror>
+        <div class="rightColumn">
+            <div class="editor">
+                <div class="codemirrorWrapper">
+                    <CodeMirror
+                        class="codemirror"
+                        bind:this={editor}
+                        {theme}
+                        extensions={vimMode ? [vim()] : []}
+                        value={DEFAULT_SHADER}
+                        lang={glsl()}
+                    ></CodeMirror>
+                </div>
+                <button aria-label="recompile">Recompile</button>
+                <button aria-label="save">Save</button>
+                <button onclick={toggleVim} aria-label="vimKeybinds">Vim</button
+                >
+            </div>
+            <div class="texturePreviews"></div>
+        </div>
     </div>
 </main>
 
 <style>
     @import "../../static/app.css";
-
     .container {
         background-color: var(--bg-color);
+        display: flex;
+        flex-direction: column;
+    }
+
+    @media (min-width: 768px) {
+        .container {
+            flex-direction: row;
+        }
     }
 
     canvas {
         image-rendering: pixelated;
-        background-color: black;
-        border: 1px solid var(--border-color);
+        width: 100%;
+        min-width: 400px;
+        max-width: 800px;
         border-radius: 4px;
         box-shadow: 0 4px 12px rgba(0, 0, 0, 0.5);
+        height: auto;
+    }
+
+    .leftColumn,
+    .rightColumn {
+        padding: 1rem;
+    }
+
+    .rightColumn {
+        flex: 1;
     }
 </style>
